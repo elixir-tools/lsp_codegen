e625fd3789e968b98c9631c55761821466e1158a
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-03-17 02:04:51 -0400

    fix: write to directory for type

--- lib/lsp_codegen.ex
+++ lib/lsp_codegen.ex
@@ -1,10 +1,17 @@
 defmodule LSPCodegen do
+  alias LSPCodegen.{
+    Enumeration,
+    Notification,
+    Request,
+    Structure,
+    TypeAlias
+  }
+
   def generate(argv) when is_list(argv) do
     {opts, _} = OptionParser.parse!(argv, strict: [path: :string])
     path = opts[:path]
 
     File.rm_rf!(path)
-    File.mkdir_p!(path)
 
     %LSPCodegen.MetaModel{} =
       metamodel =
@@ -17,6 +24,17 @@ defmodule LSPCodegen do
             metamodel.requests ++
             metamodel.notifications ++ metamodel.enumerations ++ metamodel.type_aliases do
       source_code = LSPCodegen.Codegen.to_string(mod, metamodel)
+
+      path =
+        case mod do
+          %Enumeration{} -> Path.join(path, "enumerations")
+          %Notification{} -> Path.join(path, "notifications")
+          %Request{} -> Path.join(path, "requests")
+          %Structure{} -> Path.join(path, "structures")
+          %TypeAlias{} -> Path.join(path, "type_aliases")
+        end
+
+      File.mkdir_p!(path)
 
       File.write!(
         Path.join(path, Macro.underscore(LSPCodegen.Naming.name(mod)) <> ".ex"),
